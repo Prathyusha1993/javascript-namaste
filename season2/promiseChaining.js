d6fdcae0708b3f42fb2c4a0c9c2211209f4299ff
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-10-04 16:57:27 -0700

    promise examples

--- season2/promiseChaining.js
+++ season2/promiseChaining.js
@@ -21,7 +21,20 @@ function createOrder(cart){
 
 function proccedToPayment(orderId){
     return new Promise((resolve, reject) => {
-        resolve('Payment is successfull');
+        resolve('Payment is done');
+        
+    })
+}
+
+function showOrderSummary(paymentInfo){
+    return new Promise((resolve, reject) => {
+        resolve('Order summary is shown');
+    })
+}
+
+function updateWallet(paymentInfo){
+    return new Promise((resolve, reject) => {
+        resolve('wallet is updated');
     })
 }
 
@@ -41,4 +54,40 @@ createOrder(cart)
     console.log(paymentInfo);
     return paymentInfo;
 })
+// .then((paymentInfo) => {
+//     showOrderSummary(paymentInfo);
+// })
+// .then((paymentInfo) => {
+//     console.log(paymentInfo);
+//     return paymentInfo;
+// })
+// .then((paymentInfo) => {
+//     updateWallet(paymentInfo);
+// }).then((paymentInfo) => {
+//     console.log(paymentInfo);
+// })
 .catch(error => console.log(error.message));
+
+
+//ANOTHER EXAMPLE:
+const promise1 = new Promise((resolve,reject) => {
+    if(true){
+        resolve('Uber has arrived');
+    }
+})
+
+promise1.then((message) => {
+    console.log(message);
+})
+.catch((error) => console.log(error.message));
+
+//ex:
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('I Love You');
+    }, 1000)
+});
+
+promise2.then((value) => {
+    document.getElementById('demo').innerHTML = value;
+})
